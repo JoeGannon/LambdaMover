8bcd0c14e1bbf69829e9918372f1f21d363f45a9
Joe Gannon <joegannon90@yahoo.com>
2018-09-30 22:06:45 -0400

    promote while taking a piece

--- lambda/custom/PromotePiece.cs
+++ lambda/custom/PromotePiece.cs
@@ -10,8 +10,12 @@
         {
             intent.Slots.TryGetValue("Square", out var square);
             intent.Slots.TryGetValue("Piece", out var piece);
+            intent.Slots.TryGetValue("Pawn", out var pawn);
 
             var text = square.Value.ToSquare() + "=" + piece.Value.ToPiece();
+
+            if (!string.IsNullOrEmpty(pawn?.Value))
+                text = pawn.Value.ToLower() + "x" + square.Value.ToSquare() + "=" + piece.Value.ToPiece();
 
             return text;
         }
